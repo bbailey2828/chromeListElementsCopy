000e57322ed2eb96e690ef01ddfa4661b0fa4067
bbailey2828 <bbailey2828@gmail.com>
2023-10-13 11:34:04 -0400

    Remove .history directory

--- .history/inject_20231013093348.js
+++
@@ -1,7 +0,0 @@
-const script = document.createElement('script');
-script.textContent = `
-    console.log('Direct script injection working.');
-    alert('Direct script injection working.');
-`;
-(document.head || document.documentElement).appendChild(script);
-script.remove();
